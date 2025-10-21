3b35a2a4b50c584c1b03dccd908dc55d18ee45a2
Yash-KK <reachyashk@gmail.com>
2025-10-21 21:26:18 +0530

    add dependencies-4

--- app/config.py
+++ app/config.py
@@ -1,26 +1,17 @@
+# app/config.py
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
-ALGORITHM = os.getenv("ALGORITHM")
-TOKEN_EXPIRE_MINUTES = os.getenv("TOKEN_EXPIRE_MINUTES")
-REFRESH_SECRET_KEY = os.getenv("REFRESH_SECRET_KEY")
-REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
-SYNC_DATABASE_URL = os.getenv("SYNC_DATABASE_URL")
 class Settings(BaseSettings):
-    DATABASE_URL: str = DATABASE_URL
-    SECRET_KEY: str = SECRET_KEY
-    ALGORITHM: str = ALGORITHM
-    TOKEN_EXPIRE_MINUTES: int = 30 
-    REFRESH_SECRET_KEY: str = REFRESH_SECRET_KEY
-    REFRESH_TOKEN_EXPIRE_DAYS: int = int(REFRESH_TOKEN_EXPIRE_DAYS)
-    SYNC_DATABASE_URL: str = SYNC_DATABASE_URL
-    class Config:
-        env_file = ".env"
+    SECRET_KEY: str
+    DATABASE_URL: str
+    ALGORITHM: str
+    TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_SECRET_KEY: str
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    SYNC_DATABASE_URL: str
 
+    class Config:
+        env_file = ".env"   # For local development
+        case_sensitive = True
 
 settings = Settings()
