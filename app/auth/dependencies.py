42e92c17e8f51c31ec03a40981e2c675b054efe3
Yash-KK <reachyashk@gmail.com>
2025-10-21 20:23:16 +0530

    add authenticated route

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,4 +1,4 @@
-from app.database import get_db
+from app.database import get_async_db
 from app.models import User
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -11,7 +11,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 async def get_current_user(
     token: str = Depends(oauth2_scheme),
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession = Depends(get_async_db)
 ):
     email = verify_token(token)
     stmt = select(User).where(User.email == email)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_async_db
 from app import models, schemas
+from app.auth.dependencies import get_current_active_user
 from app.auth.routers import router as auth_router
 
 app = FastAPI()
@@ -13,6 +15,14 @@ app.include_router(auth_router)
 def read_root():
     return {"status": "Image built on EC2"}
 
+@app.get("/only-auth")
+def only_auth(
+    db: AsyncSession = Depends(get_async_db),
+    current_user: models.User = Depends(get_current_active_user)
+):
+    return {
+        "status": "Yes you are authenticated"
+    }
 
 @app.get("/healthy")
 def check_health():
